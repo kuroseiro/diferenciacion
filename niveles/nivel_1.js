0a139a9caaa77b9b01963da9abf0a39b0240880e
kuroseiro <40370903+kuroseiro@users.noreply.github.com>
2019-05-30 18:40:38 -0500

    niveles

---
+++ niveles/nivel_1.js
@@ -0,0 +1,16 @@
+var detenerDrag = true;
+function drop(eve) {
+  eve.preventDefault();
+  let datoId = eve.dataTransfer.getData("text");
+  if (detenerDrag) {
+    eve.target.appendChild(document.getElementById(datoId));
+    document.location.href="nivel_3.html";
+    detenerDrag = false;
+  }
+}
+function drag(eve) {
+  eve.dataTransfer.setData("text", eve.target.id);
+}
+function permiso(eve) {
+  eve.preventDefault();
+}
